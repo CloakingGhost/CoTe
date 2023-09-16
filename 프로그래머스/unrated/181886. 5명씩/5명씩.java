74b88da550d549a9b6f435a9d87b57626338d27e
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2023-09-16 16:32:58 +0900

    [unrated] Title: 5명씩, Time: 0.01 ms, Memory: 70.9 MB -BaekjoonHub

--- 프로그래머스/unrated/181886. 5명씩/5명씩.java
+++ 프로그래머스/unrated/181886. 5명씩/5명씩.java
@@ -1,9 +1,9 @@
 class Solution {
     public String[] solution(String[] names) {
-        String[] answer = new String[((names.length - 1) / 5) + 1];
+        String[] answer = new String[(names.length - 1) / 5 + 1];
         int len = answer.length;
-        for(int i = 0, j = 0; i < len; i++, j += 5){
-            answer[i] = names[j];
+        for(int i = 0; i < len; i++){
+            answer[i] = names[i * 5];
         }
         return answer;
     }
