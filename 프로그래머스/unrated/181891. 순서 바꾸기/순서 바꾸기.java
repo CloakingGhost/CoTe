02bbffdd552754cbc6fe1673874fd6faa78343cc
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2023-09-17 18:25:21 +0900

    [unrated] Title: 순서 바꾸기, Time: 0.03 ms, Memory: 69.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
+++ 프로그래머스/unrated/181891. 순서 바꾸기/순서 바꾸기.java
@@ -1,18 +1,12 @@
 import java.util.ArrayList;
 import java.util.List;
 class Solution {
-	public Integer[] solution(int[] num_list, int n) {
+	public int[] solution(int[] num_list, int n) {
 		int len = num_list.length;
-		ArrayList<Integer> arr = new ArrayList<>() {
-			{
-				for (int i = 0; i < len; i++) {
-					if (i < len - n)
-						add(num_list[i + n]);
-					else
-						add(num_list[i - (len - n)]);
-				}
-			}
-		};
-		return arr.toArray(new Integer[len]);
+		int[] answer = new int[len];
+		for (int i = 0; i < len; i++) {
+			answer[i] = num_list[(i + n) % num_list.length];
+		}
+		return answer;
 	}
 }
