5fe7c99ff2e00dbd653f35f4e1dbc8f4a911e01a
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2023-09-17 17:22:19 +0900

    [unrated] Title: l로 만들기, Time: 12.95 ms, Memory: 88.8 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.java
@@ -1,17 +1,5 @@
 class Solution {
     public String solution(String myString) {
-        StringBuilder sb = new StringBuilder();
-        int len = myString.length();
-        char letter;
-        for (int i = 0; i < len; i++){
-            letter = myString.charAt(i);
-            if(letter < 'l'){
-                sb.append("l");
-            } else{
-                sb.append(letter);
-            }
-            
-        }
-        return sb.toString();
+        return myString.replaceAll("[^l-z]", "l");
     }
 }
