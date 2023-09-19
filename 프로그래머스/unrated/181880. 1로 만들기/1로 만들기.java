de09f7c5787a3b40dc27a5b52be5916be1758e93
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2023-09-19 23:35:01 +0900

    [unrated] Title: 1로 만들기, Time: 0.02 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
+++ 프로그래머스/unrated/181880. 1로 만들기/1로 만들기.java
@@ -1,18 +1,12 @@
 class Solution {
 	public int solution(int[] num_list) {
 		int answer = 0;
-		for (int i = 0; i < num_list.length; i++) {
-			int num = num_list[i];
+		for (int num : num_list) {
 			while (num != 1) {
-				if (num % 2 == 0) {
-					num /= 2;
-				} else {
-					num = --num / 2;
-				}
+                num /= 2;
 				answer++;
 			}
 		}
-
 		return answer;
 	}
 }
