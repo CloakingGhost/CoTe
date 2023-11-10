f385d6ed7929cfd265b6b264f8d0d8d918043fad
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2023-11-11 01:25:48 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.02 ms, Memory: 74.1 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.java
@@ -1,22 +1,12 @@
 class Solution {
 	public int solution(int num) {
-		int answer = -1;
-		int cnt = 0;
-		while (cnt < 501) {
-            if (num == 1) {
-				return cnt;
-			}
-			switch (num % 2) {
-			case 0:
-				num /= 2;
-				break;
-			case 1:
-				num *= 3;
-				num++;
-				break;
-			}
-            cnt++;
+		long n = (long) num;
+		for (int i = 0; i < 500; i++) {
+			if (n == 1)
+				return i;
+			n = (n % 2 == 0) ? n / 2 : n * 3 + 1;
 		}
-		return answer;
-	}
+
+		return -1;
+	} 
 }
