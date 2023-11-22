ab4b7f1025ad48bde9c0b35eb1a1fd9bc7da7a55
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2023-11-22 10:16:54 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.06 ms, Memory: 75.3 MB -BaekjoonHub

---
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -0,0 +1,16 @@
+class Solution {
+	public int solution(int n) {
+		StringBuilder sb = new StringBuilder();
+		while (n > 0) {
+			sb.append(n % 3);
+			n /= 3;
+		}
+		sb.reverse();
+		int size = sb.length();
+		int answer = 0;
+		for (int i = 0; i < size; i++) {
+            answer += (sb.charAt(i) - '0') * Math.pow(3, i);
+		}
+		return answer;
+	}
+}
