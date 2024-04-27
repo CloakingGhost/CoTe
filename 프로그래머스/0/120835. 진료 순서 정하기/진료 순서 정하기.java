a537f5c10766cacf28bc8672a217c8b8d016045e
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-04-27 15:05:38 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.03 ms, Memory: 81.8 MB -BaekjoonHub

--- 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.java
@@ -3,16 +3,17 @@ import java.util.HashMap;
 import java.util.Map;
 class Solution {
     public int[] solution(int[] emergency) {
-        HashMap<Integer, Integer> map = new HashMap<>(){{
-            for (int i = 0; i < emergency.length; i++) {
-                put(emergency[i], i);
-            }
-        }};
-
-        int[] idx = map.entrySet().stream().sorted((o1, o2) -> o2.getKey() - o1.getKey()).mapToInt(Map.Entry::getValue).toArray();
         int[] answer = new int[emergency.length];
-        for(int i = 0; i < idx.length; i++){
-            answer[idx[i]] = i + 1;
+
+        for(int i = 0; i < answer.length; i++){
+
+            int idx = 1;
+            for(int j = 0; j < answer.length; j++){
+                if(emergency[i] < emergency[j]){
+                    idx++;
+                }
+            }
+            answer[i] = idx;
         }
         return answer;
     }
