19141ceefb88e41e42fafb6684a769208934263b
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-16 21:22:41 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.04 ms, Memory: 93.9 MB -BaekjoonHub

--- 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
+++ 프로그래머스/0/120861. 캐릭터의 좌표/캐릭터의 좌표.java
@@ -3,27 +3,45 @@ class Solution {
     
         int[] answer = {0, 0};
 
+        // 올바른 함수 넣기
         for(String key : keyinput){
             switch(key){
                 case "up":
                     if(answer[1] + 1 <= board[1] / 2)
-                        answer[1]++;
+                        func1(answer);
                     break;
                 case "down":
                     if(answer[1] - 1 >= -(board[1] / 2))
-                        answer[1]--;
+                        func4(answer);
                     break;
                 case "right":
                     if(answer[0] + 1 <= board[0] / 2)
-                        answer[0]++;
+                        func3(answer);
                     break;
                 default:
                     if(answer[0] - 1 >= -(board[0] / 2))
-                        answer[0]--;
+                        func2(answer);
             }
         }
         
         return answer;
+    }
+    
+    
+    private void func1(int[] arr){
+        arr[1]++;
+    }
+    
+    private void func2(int[] arr){
+        arr[0]--;
+    }
+    
+    private void func3(int[] arr){
+        arr[0]++;
+    }
+    
+    private void func4(int[] arr){
+        arr[1]--;
     }
     
 }
