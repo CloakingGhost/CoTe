92c4597a0ea9c9305f541168e0cda7e9faf50c87
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-17 14:45:26 +0900

    [level 0] Title: 컨트롤 제트, Time: 0.17 ms, Memory: 81.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120853. 컨트롤 제트/컨트롤 제트.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int solution(String s) {
+        int answer = 0;
+        String[] strs = s.split(" ");
+        for(int i = strs.length - 1; i >= 0; i--){
+            if(strs[i].equals("Z")){
+                i--;
+            }else {
+                answer += Integer.parseInt(strs[i]);
+            }
+        }
+        return answer;
+    }
+}
