0a588ba0e5d19234ed6db78d53f46c890751d645
CloakingGhost <98940420+CloakingGhost@users.noreply.github.com>
2024-08-17 14:27:32 +0900

    [level 0] Title: 평행, Time: 0.03 ms, Memory: 76.4 MB -BaekjoonHub

--- 프로그래머스/0/120875. 평행/평행.java
+++ 프로그래머스/0/120875. 평행/평행.java
@@ -3,14 +3,13 @@ class Solution {
         for (int i = 0; i < 4; i++) {
             float slope1 = slope(dots[i], dots[(i + 1) % 4]);
             float slope2 = slope(dots[(i + 2) % 4], dots[(i + 3) % 4]);
-
             if (isParallel(slope1, slope2)) return 1;
         }
         return 0;
     }
 
     public float slope(int[] dot1, int[] dot2) {
-        return (float) (dot2[0] - dot1[0]) / (dot2[1] - dot1[1]);
+        return (float) (dot2[1] - dot1[1]) / (dot2[0] - dot1[0]);
     }
 
     private boolean isParallel(float slope1, float slope2) {
