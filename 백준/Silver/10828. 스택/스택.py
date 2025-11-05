095b704c36d12cc81fcd5fa89eb6f15d826a62ca
Jaehyeon Lee <project0858@gmail.com>
2025-11-05 21:51:45 +0900

    [Silver IV] Title: 스택, Time: 40 ms, Memory: 33432 KB -BaekjoonHub

---
+++ 백준/Silver/10828. 스택/스택.py
@@ -0,0 +1,17 @@
+import sys
+
+input = sys.stdin.readline
+n = int(input())
+stack = []
+for _ in range(n):
+    command = input().rstrip()
+    if command == "pop":
+        print(stack.pop() if stack else -1)
+    elif command == "size":
+        print(len(stack))
+    elif command == "empty":
+        print(0 if stack else 1)
+    elif command == "top":
+        print(stack[-1] if stack else -1)
+    else :
+        stack.append(command.split()[-1])
