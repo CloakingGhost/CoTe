8d4319c1198f64cc732ec446c690af00ba6a45f8
Jaehyeon Lee <project0858@gmail.com>
2025-11-27 17:00:46 +0900

    [Silver III] Title: 블로그, Time: 164 ms, Memory: 58224 KB -BaekjoonHub

---
+++ 백준/Silver/21921. 블로그/블로그.py
@@ -0,0 +1,30 @@
+import sys
+
+input = sys.stdin.readline
+
+n, x = map(int, input().split())
+visited = list(map(int, input().split()))
+
+left, right = -1, x - 1
+temp = max_visited = max(0, sum(visited[:x]))
+cnt = 1
+
+while right + 1 < n:
+    left += 1
+    right += 1
+
+    temp -= visited[left]
+    temp += visited[right]
+
+
+    if max_visited < temp:
+        max_visited = temp
+        cnt = 1
+    elif max_visited == temp:
+        cnt += 1
+
+if max_visited:
+    print(max_visited)
+    print(cnt)
+else:
+    print("SAD")
