d827af4553d89b8ece58b9ecdfc5c810557c19f6
Jaehyeon Lee <project0858@gmail.com>
2025-11-28 20:42:38 +0900

    [Gold V] Title: 두 용액, Time: 112 ms, Memory: 44748 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.py
+++ 백준/Gold/2470. 두 용액/두 용액.py
@@ -5,22 +5,22 @@ input = sys.stdin.readline
 n = int(input())
 solutions = list(map(int, input().split()))
 solutions.sort()
-answer = (0, 0)
-
 
 left, right = 0, n - 1
 tmp = float("INF")
+answer = (0, 0)
+
 while left < right:
     current = solutions[left] + solutions[right]
     if abs(current) < tmp:
         tmp = abs(current)
         answer = (solutions[left], solutions[right])
+        if current == 0:
+            break
     
     if current < 0:
         left += 1
-    elif current > 0:
-        right -= 1
     else:
-        answer = (solutions[left], solutions[right])
-        break
+        right -= 1
+
 print(*answer)
