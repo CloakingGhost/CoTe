24de02ade881c6cb01e653b58c013c8e2900d0b7
Jaehyeon Lee <project0858@gmail.com>
2025-12-19 11:34:13 +0900

    [Gold V] Title: 토마토, Time: 1872 ms, Memory: 52500 KB -BaekjoonHub

--- 백준/Gold/7569. 토마토/토마토.py
+++ 백준/Gold/7569. 토마토/토마토.py
@@ -5,41 +5,47 @@ from collections import deque
 from sys import stdin
 
 input = stdin.readline
+EMPTY, UNRIPE, RIPE = -1, 0, 1
 
 
-def is_in_area(nh, nr, nc, max_h, n, m):
+def is_in_bounds(nh, nr, nc, max_h, n, m):
     return 0 <= nh < max_h and 0 <= nr < n and 0 <= nc < m
+
+
+def has_unripe(matrix):
+    return any(UNRIPE in r for h in matrix for r in h)
 
 
 def bfs(tomatos, matrix, max_h, n, m):
     # 6방향
     dh, dr, dc = [0, 0, 0, 0, 1, -1], [0, 0, 1, -1, 0, 0], [1, -1, 0, 0, 0, 0]
+    direction = len(dh)
     queue = deque(tomatos)
     result = 0
     while queue:
         h, r, c, t = queue.popleft()
 
-        for d in range(6):
+        for d in range(direction):
             nh, nr, nc = h + dh[d], r + dr[d], c + dc[d]
-            if is_in_area(nh, nr, nc, max_h, n, m) and matrix[nh][nr][nc] == 0:
-                matrix[nh][nr][nc] = 1
+            if is_in_bounds(nh, nr, nc, max_h, n, m) and matrix[nh][nr][nc] == UNRIPE:
+                matrix[nh][nr][nc] = RIPE
                 queue.append((nh, nr, nc, t + 1))
 
-            if not queue:
-                result = t
+        if not queue:
+            result = max(result, t)
 
-    return -1 if any(0 in r for h in matrix for r in h) else result
+    return -1 if has_unripe(matrix) else result
 
-# max_h 재할당 방지
 m, n, max_h = map(int, input().split())
 matrix = [[list(map(int, input().split())) for _ in range(n)] for _ in range(max_h)]
 
+TIME = 0
 tomatos = [
-    (i, j, k, 0)
+    (i, j, k, TIME)
     for i in range(max_h)
     for j in range(n)
     for k in range(m)
-    if matrix[i][j][k] == 1
+    if matrix[i][j][k] == RIPE
 ]
 
 print(bfs(tomatos, matrix, max_h, n, m))
