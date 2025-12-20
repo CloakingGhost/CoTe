3cf73505ea7b9c423d8b8050ab97316efbf16b64
Jaehyeon Lee <project0858@gmail.com>
2025-12-20 23:33:42 +0900

    [Gold IV] Title: 연구소, Time: 1200 ms, Memory: 35036 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -5,37 +5,43 @@ input = stdin.readline
 EMPTY, WALL, VIRUS = 0, 1, 2
 
 
-def bfs(matrix, virus_pos):
+def bfs(matrix, virus_pos, max_safe_count):
     dy, dx = [1, -1, 0, 0], [0, 0, 1, -1]
     queue = deque(virus_pos)
     tmp_matrix = [r[:] for r in matrix]
+    potential_safe = INIT_EMPTY_COUNT - MAX_WALL_COUNT
     total_added_virus = 0
     while queue:
         y, x = queue.popleft()
-
+        
+        
         for d in range(4):
             ny, nx = y + dy[d], x + dx[d]
             if 0 <= ny < n and 0 <= nx < m and tmp_matrix[ny][nx] == EMPTY:
-                queue.append((ny, nx))
                 tmp_matrix[ny][nx] = VIRUS
                 total_added_virus += 1
-    return INIT_EMPTY_COUNT - MAX_WALL_COUNT - total_added_virus
+                queue.append((ny, nx))
+                
+                if potential_safe - total_added_virus <= max_safe_count:
+                    return -1
+
+    return potential_safe - total_added_virus
 
 
 def dfs(wall_count, start):
-    max_safe_count = 0
+    global max_safe_count
+
     if wall_count == MAX_WALL_COUNT:
-        return bfs(matrix, virus_pos)
+        safe_count = bfs(matrix, virus_pos, max_safe_count)
+        max_safe_count = max(max_safe_count, safe_count)
+        return
 
     for i in range(start, INIT_EMPTY_COUNT):
         y, x = empty_pos[i]
 
         matrix[y][x] = WALL
-        safe_count = dfs(wall_count + 1, i + 1)
-        max_safe_count = max(max_safe_count, safe_count)
+        dfs(wall_count + 1, i + 1)
         matrix[y][x] = EMPTY
-
-    return max_safe_count
 
 
 n, m = map(int, input().split())
@@ -52,5 +58,6 @@ for i in range(n):
             virus_pos.append((i, j))
 
 INIT_EMPTY_COUNT = len(empty_pos)
-
-print(dfs(0, 0))
+max_safe_count = 0
+dfs(0, 0)
+print(max_safe_count)
