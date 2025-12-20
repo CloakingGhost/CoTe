8e68d5e0882a5020d0d46ba27ef8c969ee7d504c
Jaehyeon Lee <project0858@gmail.com>
2025-12-20 23:10:39 +0900

    [Gold IV] Title: 연구소, Time: 1420 ms, Memory: 35148 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.py
+++ 백준/Gold/14502. 연구소/연구소.py
@@ -1,6 +1,3 @@
-# https://www.acmicpc.net/problem/14502
-# 연구소
-
 from sys import stdin
 from collections import deque
 
@@ -8,54 +5,43 @@ input = stdin.readline
 EMPTY, WALL, VIRUS = 0, 1, 2
 
 
-def is_in_bounds(y, x, n, m):
-    return 0 <= y < n and 0 <= x < m
+def bfs(matrix, virus_pos):
+    dy, dx = [1, -1, 0, 0], [0, 0, 1, -1]
+    queue = deque(virus_pos)
+    tmp_matrix = [r[:] for r in matrix]
+    total_added_virus = 0
+    while queue:
+        y, x = queue.popleft()
+
+        for d in range(4):
+            ny, nx = y + dy[d], x + dx[d]
+            if 0 <= ny < n and 0 <= nx < m and tmp_matrix[ny][nx] == EMPTY:
+                queue.append((ny, nx))
+                tmp_matrix[ny][nx] = VIRUS
+                total_added_virus += 1
+    return INIT_EMPTY_COUNT - MAX_WALL_COUNT - total_added_virus
 
 
-def dfs(count, matrix, virus, empty_pos, start):
+def dfs(wall_count, start):
+    max_safe_count = 0
+    if wall_count == MAX_WALL_COUNT:
+        return bfs(matrix, virus_pos)
 
-    if count == MAX_WALL_COUNT:
-        return bfs(matrix, virus)
-
-    max_safe_area = 0
-
-    for i in range(start, EMPTY_COUNT):
+    for i in range(start, INIT_EMPTY_COUNT):
         y, x = empty_pos[i]
 
-        matrix[y][x] = WALL
-        safe_count = dfs(count + 1, matrix, virus, empty_pos, i + 1)
-        max_safe_area = max(max_safe_area, safe_count)
+        matrix[y][x] = VIRUS
+        safe_count = dfs(wall_count + 1, i + 1)
+        max_safe_count = max(max_safe_count, safe_count)
         matrix[y][x] = EMPTY
 
-    return max_safe_area
-
-
-def bfs(matrix, virus):
-    dy, dx = [1, -1, 0, 0], [0, 0, 1, -1]
-    direction = len(dy)
-    queue = deque(virus)
-    tmp_matrix = [row[:] for row in matrix]
-
-    virus_count = 0
-    while queue:
-
-        y, x = queue.popleft()
-        for d in range(direction):
-            ny, nx = y + dy[d], x + dx[d]
-            if is_in_bounds(ny, nx, n, m) and tmp_matrix[ny][nx] == EMPTY:
-                if tmp_matrix[ny][nx] == EMPTY:
-                    tmp_matrix[ny][nx] = VIRUS
-                    virus_count += 1
-                    queue.append((ny, nx))
-
-    return EMPTY_COUNT - virus_count - MAX_WALL_COUNT
+    return max_safe_count
 
 
 n, m = map(int, input().split())
-matrix = []
-empty_pos = []
-virus_pos = []
+empty_pos, virus_pos, matrix = [], [], []
 
+MAX_WALL_COUNT = 3
 for i in range(n):
     row = list(map(int, input().split()))
     matrix.append(row)
@@ -65,7 +51,6 @@ for i in range(n):
         elif row[j] == VIRUS:
             virus_pos.append((i, j))
 
-WALL_COUNT_INIT = 0
-MAX_WALL_COUNT = 3
-EMPTY_COUNT = len(empty_pos)
-print(dfs(WALL_COUNT_INIT, matrix, virus_pos, empty_pos, 0))
+INIT_EMPTY_COUNT = len(empty_pos)
+
+print(dfs(0, 0))
