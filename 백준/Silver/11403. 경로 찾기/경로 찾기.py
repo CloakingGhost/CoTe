90ef8b5a723c9d3f76761b60be049ce56e7fc005
Jaehyeon Lee <project0858@gmail.com>
2025-12-31 00:26:57 +0900

    [Silver I] Title: 경로 찾기, Time: 88 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,15 +1,49 @@
+# https://www.acmicpc.net/problem/11403
+# 경로 찾기
+
 from sys import stdin
+from collections import deque
+
+
+def bfs(i, n, graph):
+    queue = deque([i])
+    visited = [0] * n
+
+    while queue:
+        s = queue.popleft()
+        for ni, val in enumerate(graph[s]):
+
+            if val and not visited[ni]:
+                visited[ni] = 1
+                queue.append(ni)
+    return visited
+
+
 input = stdin.readline
 n = int(input())
 
 # 방향 그래프
 graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
-# 플로이드-워셜 방식 (핵심 로직 3줄)
-for k in range(n):        # 거쳐가는 노드
-    for i in range(n):    # 출발 노드
-        for j in range(n):# 도착 노드
-            if graph[i][k] and graph[k][j]:
-                graph[i][j] = 1
 
-for row in graph:
+
+answer = [bfs(i, n, graph) for i in range(n)]
+
+
+for row in answer:
     print(*row)
+
+# from sys import stdin
+# input = stdin.readline
+# n = int(input())
+
+# # 방향 그래프
+# graph = [list(map(int, input().rstrip().split())) for _ in range(n)]
+# # 플로이드-워셜 방식 (핵심 로직 3줄)
+# for k in range(n):        # 거쳐가는 노드
+#     for i in range(n):    # 출발 노드
+#         for j in range(n):# 도착 노드
+#             if graph[i][k] and graph[k][j]:
+#                 graph[i][j] = 1
+
+# for row in graph:
+#     print(*row)
