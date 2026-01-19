128bf0ade17cb10d26b8548b350a264ca20be412
Jaehyeon Lee <project0858@gmail.com>
2026-01-19 12:28:50 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 344 ms, Memory: 54216 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.py
@@ -0,0 +1,13 @@
+from sys import stdin
+
+input = stdin.readline
+N = int(input())
+
+sort_key = lambda x: (x[1], x[0])
+
+
+coordinate = [tuple(map(int, input().split())) for _ in range(N)]
+coordinate.sort(key=sort_key)
+
+for e in coordinate:
+    print(*e)
