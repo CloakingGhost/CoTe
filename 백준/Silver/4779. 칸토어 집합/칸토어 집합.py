e458457e338cd8ad0f772a381d91636708e65674
Jaehyeon Lee <project0858@gmail.com>
2026-02-22 15:34:55 +0900

    [Silver III] Title: 칸토어 집합, Time: 64 ms, Memory: 39848 KB -BaekjoonHub

---
+++ 백준/Silver/4779. 칸토어 집합/칸토어 집합.py
@@ -0,0 +1,28 @@
+# https://www.acmicpc.net/problem/4779
+
+import sys
+
+input = sys.stdin.readline
+
+
+def change_black(dash, s, length):
+    if length == 1:
+        return
+
+    sep = length // 3
+
+    # 왼쪽
+    change_black(dash, s, sep)
+    # 중앙
+    dash[s + sep : s + sep * 2] = [" "] * sep
+    # 오른쪽
+    change_black(dash, s + sep * 2, sep)
+
+
+for line in sys.stdin:
+    N = int(line)
+    length = 3**N
+    dash = list("-" * length)
+
+    change_black(dash, 0, length)
+    print("".join(dash))
