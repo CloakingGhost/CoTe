c3be1e646cc34898f1c045145f26c9d4ce3df8e7
Jaehyeon Lee <project0858@gmail.com>
2026-05-12 00:40:34 +0900

    [level 1] Title: 대충 만든 자판, Time: 0.46 ms, Memory: 9.25 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.py
@@ -1,29 +1,27 @@
 # keymap을 한번에 조회하도록 만들어야 함
-# key들을 한번씩은 조회하고 최소 인덱스를 별도의 배열에 저장
+# key들을 한번씩은 조회하고 최소 인덱스를 별도의 dict에 저장
 def solution(keymap, targets):
-    # [keymap_idx, char_idx]
-    keylist = [(101, 101)] * 26
+    keydict = {}
 
     # 키맵에서 문자열꺼내
-    # 문제열에서 문자꺼내
-    # 그 문자의 인덱스를 별도의 배열에 저장할건데
-    # 지금 문자의 인덱스가 별도 배열에 저장된 인덱스보다 작으면 교체
-    # 배열을 찾아갈 때는 ord(문자) -65을 하면 인덱스가 나옴
+    # 문자열에서 문자꺼내
+    # dict에 문자가 없거나 인덱스가 더 작으면 인덱스 + 1 저장 
 
-    for i, key in enumerate(keymap):
-        for j, k in enumerate(key):
-            if j < keylist[ord(k) - 65][1]:
-                keylist[ord(k) - 65] = (i, j + 1)
+    for key in keymap:
+        for i, c in enumerate(key):
+            if c not in keydict or i + 1 < keydict[c]:
+                keydict[c] = i + 1
 
     answer = []
+    
     for t in targets:
         count = 0
         for c in t:
-            keymap_idx, char_idx = keylist[ord(c) - 65]
-            if keymap_idx != 101:
-                count += char_idx
-            else:
+            if c not in keydict:
                 count = -1
                 break
+
+            count += keydict[c]
+
         answer.append(count)
     return answer
