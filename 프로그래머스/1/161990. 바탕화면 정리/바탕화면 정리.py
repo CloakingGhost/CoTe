45da5703d5a07e6c066cadb1d8df407a39074909
Jaehyeon Lee <project0858@gmail.com>
2026-05-15 15:54:36 +0900

    [level 1] Title: 바탕화면 정리, Time: 0.21 ms, Memory: 9.03 MB -BaekjoonHub

--- 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
+++ 프로그래머스/1/161990. 바탕화면 정리/바탕화면 정리.py
@@ -1,56 +1,10 @@
 def solution(wallpaper):
-    m, n = len(wallpaper), len(wallpaper[0])
-    
-    # 상단좌측, 좌측상단, 우측하단, 하단우측
-    location: list[tuple[int, int]] = []
-    
-    isStop = False
-    # 상단좌측
-    for i in range(m):
-        for j in range(n):
-            if wallpaper[i][j] == "#":
-                location.append((i, j))
-                isStop = True
-                break
-        if isStop:
-            isStop = False
-            break
-    # 좌측상단
-    for i in range(n):
-        for j in range(m):
-            if wallpaper[j][i] == "#":
-                location.append((j, i))
-                isStop = True
-                break
-        if isStop:
-            isStop = False
-            break
-    # 우측하단
-    for i in range(m - 1, -1, -1):
-        for j in range(n - 1, -1, -1):
-            if wallpaper[i][j] == "#":
-                location.append((i, j))
-                isStop = True
-                break
-        if isStop:
-            isStop = False
-            break
-    # 하단우측
-    for i in range(n - 1, -1, -1):
-        for j in range(m - 1, -1, -1):
-            if wallpaper[j][i] == "#":
-                location.append((j, i))
-                isStop = True
-                break
-        if isStop:
-            isStop = False
-            break
-        
-    # 시작점, 끝점
-    s = [min(location[0][0], location[1][0]), min(location[0][1], location[1][1])]
-    e = [
-        max(location[2][0], location[3][0]) + 1,
-        max(location[2][1], location[3][1]) + 1,
-    ]
+    a, b = [], []
 
-    return s + e
+    for i in range(len(wallpaper)):
+        for j in range(len(wallpaper[0])):
+            if wallpaper[i][j] == "#":
+                a.append(i)
+                b.append(j)
+
+    return [min(a), min(b), max(a) + 1, max(b) + 1]
